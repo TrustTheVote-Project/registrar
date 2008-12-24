3b9e4f01442758d09b276eabb879b93cf67a6062
Sean Beckett <seanb@bosworth.(none)>
2008-12-24 12:30:51 -0800

    sb: updating copy and status messages

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,10 +7,17 @@ class Registration < ActiveRecord::Base
   attr_accessor :activity_comment
 
   state :draft
-  state :submitted, :enter => Proc.new {|r| r.add_activity('Registration submitted', false)}
-  state :received, :enter => Proc.new {|r| r.add_activity('Registration received')}
-  state :rejected, :enter => Proc.new {|r| r.add_activity('Registration rejected')}
-  state :approved, :enter => Proc.new {|r| r.add_activity('Registration approved')}
+  state :submitted, :enter => Proc.new {|r| r.add_activity('Registration form has not yet arrived in the mail.
+    Next Step: After your signed original voter registration form is received at the Adams County Board of Elections,
+    the processing of your voter registration request will begin.', false)}
+  state :received, :enter => Proc.new {|r| r.add_activity('Registration form received from the USPS.
+    Next Step: A registration clerk should begin the processing of your form shortly.')}
+  state :rejected, :enter => Proc.new {|r| r.add_activity('Registration form rejected due to inadequate or missing signature.
+    Next Step: Your request cannot be processed further because it did not contain a valid signature. Please print and
+    properly sign your registration form and resubmit it to the Adams Country Board of Elections.')}
+  state :approved, :enter => Proc.new {|r| r.add_activity('Registration form validated, awaiting approval.
+    Next Step: Your request form and signature have been accepted. Your voter registration request is pending
+    completion of a cross-check of your information with the Department of Motor Vehicles.')}
 
   event :submit do
     transitions :to => :submitted, :from => :draft
@@ -39,7 +46,7 @@ class Registration < ActiveRecord::Base
     activities.create(
         :message => message,
         :clerk => set_clerk ? acting_clerk : nil,
-        :location => ['District 7 branch', 'Jefferson Avenue district branch', 'Madison Street depot'].rand,
+        :location => ['Adams County Board of Elections, 532 Tower Road', 'Adams County Records Offices, 89 Courthouse Way', 'Office of the Adams County Recorder, 731 Madison St.'].rand,
         :ip_address => "#{[123, 234, 456].rand}.#{[987, 876, 654].rand}.#{[4, 5, 6].rand}.#{[6, 7, 8].rand}",
         :comment => activity_comment
     )
